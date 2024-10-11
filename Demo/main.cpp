07c425efa3e773218c05f371675530d0c52aa447
PylaevaSvetlana <pylaeva.sweta@yandex.ru>
2024-10-11 16:22:14 +0300

    Сделала задание №2

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -1,6 +1,20 @@
-#include<iostream>
+#include <iostream>
+#include "..\Solver\Queue.h"
 
 int main()
 {
+	Queue::Queue<int> list = { 1, 2, 3, 4, 5 };
+	std::cout << list << std::endl;
+	list.enqueue(6);
+	std::cout << list << std::endl;
+	list.dequeue();
+	std::cout << list << std::endl;
+	std::cout << list.peek() << std::endl;
+	Queue::Queue<int> list1 = { 1, 2, 3, 4, 5 };
+	Queue::Queue<int> list2 = { 1, 2, 3, 4, 5 };
+	Queue::Queue<int> queue1(std::move(list1));
+	Queue::Queue<int> queue2 = std::move(list2);
+	std::cout << queue1 << std::endl << queue2 << std::endl;
+	std::cout << list1 << std::endl << list2 << std::endl;
 	return 0;
 }
--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,16 +1,244 @@
 ﻿#include "pch.h"
 #include "CppUnitTest.h"
+#include "..\Solver\Queue.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace Test
+namespace Tests
 {
-	TEST_CLASS(Test)
+	TEST_CLASS(QueueTest)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(EnqueueInt_ValidData_Success)
 		{
+			Queue::Queue<int> queue;
+			queue.enqueue(5);
+			Assert::AreEqual(queue.peek(), 5);
+		}
+
+		TEST_METHOD(EnqueueInt_ValidData_Fail)
+		{
+			Queue::Queue<int> queue;
+			queue.enqueue(5);
+			Assert::AreNotEqual(queue.peek(), 3);
+		}
+
+		TEST_METHOD(DequeueInt_ValidData_Success)
+		{
+			Queue::Queue<int> queue{ 1,2,3,4,5 };
+			queue.dequeue();
+			Assert::AreEqual(queue.peek(), 2);
+		}
+
+		TEST_METHOD(DequeueInt_ValidData_Fail)
+		{
+			Queue::Queue<int> queue{ 1,2,3,4,5 };
+			queue.dequeue();
+			Assert::AreNotEqual(queue.peek(), 3);
+		}
+
+		TEST_METHOD(СopyСonstructorInt_ValidData_Success)
+		{
+			Queue::Queue<int> queue1{ 1,2,3 }, queue2(queue1);
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+			queue1.dequeue();
+			queue2.dequeue();
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+			queue1.dequeue();
+			queue2.dequeue();
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+		}
+
+		TEST_METHOD(MoveConstructorInt_ValidData_Success)
+		{
+			Queue::Queue<int> queue1{ 1,2,3 }, queue2 = queue1, queue3(std::move(queue2));
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+			queue1.dequeue();
+			queue3.dequeue();
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+			queue1.dequeue();
+			queue3.dequeue();
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+		}
+
+		TEST_METHOD(OperatorEqualCopyingInt_ValidData_Success)
+		{
+			Queue::Queue<int> queue1{ 1,2,3 }, queue2 = queue1;
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+			queue1.dequeue();
+			queue2.dequeue();
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+			queue1.dequeue();
+			queue2.dequeue();
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+		}
+
+		TEST_METHOD(OperatorEqualMovingInt_ValidData_Success)
+		{
+			Queue::Queue<int> queue1{ 1,2,3 }, queue2 = queue1, queue3 = std::move(queue2);
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+			queue1.dequeue();
+			queue3.dequeue();
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+			queue1.dequeue();
+			queue3.dequeue();
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+		}
+
+		TEST_METHOD(EnqueueDouble_ValidData_Success)
+		{
+			Queue::Queue<double> queue;
+			queue.enqueue(5.5);
+			Assert::AreEqual(queue.peek(), 5.5);
+		}
+
+		TEST_METHOD(EnqueueDouble_ValidData_Fail)
+		{
+			Queue::Queue<double> queue;
+			queue.enqueue(5.5);
+			Assert::AreNotEqual(queue.peek(), 3.3);
+		}
+
+		TEST_METHOD(DequeueDouble_ValidData_Success)
+		{
+			Queue::Queue<double> queue{ 1.1,2.2,3.3,4.4,5.5 };
+			queue.dequeue();
+			Assert::AreEqual(queue.peek(), 2.2);
+		}
+
+		TEST_METHOD(DequeueDouble_ValidData_Fail)
+		{
+			Queue::Queue<double> queue{ 1.1,2.2,3.3,4.4,5.5 };
+			queue.dequeue();
+			Assert::AreNotEqual(queue.peek(), 3.3);
+		}
+
+		TEST_METHOD(СopyСonstructorDouble_ValidData_Success)
+		{
+			Queue::Queue<double> queue1{ 1.1,2.2,3.3 }, queue2(queue1);
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+			queue1.dequeue();
+			queue2.dequeue();
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+			queue1.dequeue();
+			queue2.dequeue();
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+		}
+
+		TEST_METHOD(MoveConstructorDouble_ValidData_Success)
+		{
+			Queue::Queue<double> queue1{ 1.1,2.2,3.3 }, queue2 = queue1, queue3(std::move(queue2));
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+			queue1.dequeue();
+			queue3.dequeue();
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+			queue1.dequeue();
+			queue3.dequeue();
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+		}
+
+		TEST_METHOD(OperatorEqualCopyingDouble_ValidData_Success)
+		{
+			Queue::Queue<double> queue1{ 1.1,2.2,3.3 }, queue2 = queue1;
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+			queue1.dequeue();
+			queue2.dequeue();
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+			queue1.dequeue();
+			queue2.dequeue();
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+		}
+
+		TEST_METHOD(OperatorEqualMovingDouble_ValidData_Success)
+		{
+			Queue::Queue<double> queue1{ 1.1,2.2,3.3 }, queue2 = queue1, queue3 = std::move(queue2);
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+			queue1.dequeue();
+			queue3.dequeue();
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+			queue1.dequeue();
+			queue3.dequeue();
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+		}
+		TEST_METHOD(EnqueueString_ValidData_Success)
+		{
+			Queue::Queue < std::string > queue;
+			queue.enqueue("abc");
+			std::string word = "abc";
+			Assert::AreEqual(queue.peek(), word);
+		}
+
+		TEST_METHOD(EnqueueString_ValidData_Fail)
+		{
+			Queue::Queue<std::string> queue;
+			queue.enqueue("abc");
+			std::string word = "cbd";
+			Assert::AreNotEqual(queue.peek(), word);
+		}
+
+		TEST_METHOD(DequeueString_ValidData_Success)
+		{
+			Queue::Queue<std::string> queue{ "a","b","c","d","e"};
+			queue.dequeue();
+			std::string word = "b";
+			Assert::AreEqual(queue.peek(), word);
+		}
+
+		TEST_METHOD(DequeueString_ValidData_Fail)
+		{
+			Queue::Queue<std::string> queue{ "a","b","c","d","e" };
+			queue.dequeue();
+			std::string word = "c";
+			Assert::AreNotEqual(queue.peek(), word);
+		}
+
+		TEST_METHOD(СopyСonstructorString_ValidData_Success)
+		{
+			Queue::Queue<std::string> queue1{ "a","b","c" }, queue2(queue1);
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+			queue1.dequeue();
+			queue2.dequeue();
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+			queue1.dequeue();
+			queue2.dequeue();
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+		}
+
+		TEST_METHOD(MoveConstructorString_ValidData_Success)
+		{
+			Queue::Queue<std::string> queue1{ "a","b","c" }, queue2 = queue1, queue3(std::move(queue2));
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+			queue1.dequeue();
+			queue3.dequeue();
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+			queue1.dequeue();
+			queue3.dequeue();
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+		}
+
+		TEST_METHOD(OperatorEqualCopyingString_ValidData_Success)
+		{
+			Queue::Queue<std::string> queue1{ "a","b","c" }, queue2 = queue1;
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+			queue1.dequeue();
+			queue2.dequeue();
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+			queue1.dequeue();
+			queue2.dequeue();
+			Assert::AreEqual(queue1.peek(), queue2.peek());
+		}
+
+		TEST_METHOD(OperatorEqualMovingString_ValidData_Success)
+		{
+			Queue::Queue<std::string> queue1{ "a","b","c" }, queue2 = queue1, queue3 = std::move(queue2);
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+			queue1.dequeue();
+			queue3.dequeue();
+			Assert::AreEqual(queue1.peek(), queue3.peek());
+			queue1.dequeue();
+			queue3.dequeue();
+			Assert::AreEqual(queue1.peek(), queue3.peek());
 		}
 	};
 }
